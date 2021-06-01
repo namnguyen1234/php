7f2f66ba84d845e22b27e5647f061ca1aaa7cfcd
namnguyen1234 <71134954+namnguyen1234@users.noreply.github.com>
2021-06-01 09:38:25 +0700

    đọc xml bằng php

---
+++ boctachxml/content.php
@@ -0,0 +1,18 @@
+require_once 'student.xml';
+<?php
+    $document = new DOMdocument();
+    $document -> load('student.xml');
+    $root = $document -> documentElement;
+    $content = $root -> textcontent;
+    echo 'root='.$content; 
+    $listchilds = $root -> childsNodes;
+    foreach ($listchilds as $childs){
+        if ($childs->nodeType == XML_ELEMENT_NODE);
+            echo '<br>' .$childs -> nodeValue;
+    }
+    $liststudents = $root->getElementsByTagName('students');
+    foreach($liststudents as $stu){
+        if ($stu->nodeType == XML_ELEMENT_NODE);
+            echo '<br>' .$stu -> nodeValue;
+    }
+?>
