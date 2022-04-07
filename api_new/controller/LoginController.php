d857c653ff876c54f408d19f4ef68faf776b16e9
namnguyen1234 <nam.nguyentien_tmp>
2022-04-07 15:14:36 +0700

    q

---
+++ api_new/controller/LoginController.php
@@ -0,0 +1,64 @@
+<?php
+require_once 'models/User.php';
+
+class LoginController
+{
+    public $content;
+    public $error;
+
+    public function login() {
+        if (isset($_SESSION['user'])) {
+            return 'Bạn đã đăng nhập';
+        }
+        if (isset($_POST['submit'])) {
+            $username = $_POST['username'];
+            $password = md5($_POST['password']);
+            if (empty($username) || empty($password)) {
+                $this->error = 'Username hoặc password không được để trống';
+            }
+            $user_model = new User();
+            if (empty($this->error)) {
+                $user = $user_model->getUserByUsernameAndPassword($username, $password);
+                if (empty($user)) {
+                    $this->error = 'Sai username hoặc password';
+                } else {
+                    $_SESSION['success'] = 'Đăng nhập thành công';
+                    $_SESSION['user'] = $user;
+                }
+            }
+                        return json_encode($user_model);
+        }
+    }
+
+   
+    public function register() {
+
+        if (isset($_POST['submit'])) {
+            $user_model = new User();
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $password_confirm = $_POST['password_confirm'];
+            $user = $user_model->getUserByUsername($username);
+            if (empty($username) || empty($password) || empty($password_confirm)) {
+                $this->error = 'Không được để trống các trường';
+            } else if ($password != $password_confirm) {
+                $this->error = 'Password nhập lại chưa đúng';
+            } else if (!empty($user)) {
+                $this->error = 'Username này đã tồn tại';
+            }
+            if (empty($this->error)) {
+
+                $user_model->username = $username;
+                $user_model->password = md5($password);
+                $user_model->status = 1;
+                $is_insert = $user_model->insertRegister();
+                if ($is_insert) {
+                    $_SESSION['success'] = 'Đăng ký thành công';
+                } else {
+                    $_SESSION['error'] = 'Đăng ký thất bại';
+                }
+            }
+        }
+                  return json_encode($user_model);
+    }
+}
